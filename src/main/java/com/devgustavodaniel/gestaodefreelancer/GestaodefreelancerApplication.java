d4ead6d378b41c320efbd579d05bcb87f6bc1243
GustavoSDaniel <gustavosdaniel@hotmail.com>
2025-05-05 19:39:38 -0300

    Iniciando Projeto

---
+++ src/main/java/com/devgustavodaniel/gestaodefreelancer/GestaodefreelancerApplication.java
@@ -0,0 +1,13 @@
+package com.devgustavodaniel.gestaodefreelancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodefreelancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodefreelancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devgustavodaniel/gestaodefreelancer/GestaodefreelancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgustavodaniel.gestaodefreelancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaodefreelancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
